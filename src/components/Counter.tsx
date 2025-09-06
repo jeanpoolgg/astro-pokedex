149ffbb3849c0d3b1f70c6fba0ccedf6ab92f905
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-09-05 21:38:56 -0500

    feat(islands): pass props and render components inside islands

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,18 @@
-import { createSignal } from "solid-js"
+import { createSignal, type Component, type JSX } from "solid-js"
 
-const Counter = () => {
-    const [counter, setCounter] = createSignal(10)
+interface Props {
+    initValue: number;
+    children?: JSX.Element;
+}
+
+const Counter: Component<Props> = (props) => {
+    const [counter, setCounter] = createSignal(props.initValue)
 
     return (
         <>
-            <h1>Counter</h1>
+            {props.children}
             <p>Values {counter()}</p>
-            <button onClick={() => setCounter(prev => ++prev) }  class="bg-blue-500 p-2 mr-2 rounded">
+            <button onClick={() => setCounter(prev => ++prev)} class="bg-blue-500 p-2 mr-2 rounded">
                 +1
             </button>
             <button onClick={() => setCounter(prev => --prev)} class="bg-blue-500 p-2 mr-2 rounded" >
